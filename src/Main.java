3c1b8d8f92f332aeb5859c602e3b829e5a00cfe4
weistapki <weistapki@gmail.com>
2023-04-26 16:00:08 +0200

    answer

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println("III : " + solution.romanToInt("III"));
+        System.out.println("LVIII : " + solution.romanToInt("LVIII"));
+        System.out.println("MCMXCIV : " + solution.romanToInt("MCMXCIV"));
+        System.out.println("IV : " + solution.romanToInt("IV"));
+    }
 }
